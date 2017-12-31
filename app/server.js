267fa87a531110fa8d85af343c34e0eb4c785ccc
Valerie Dryden <outragedpinkracoon@gmail.com>
2017-12-31 22:09:19 +0000

    renames env var

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 
 const app = express()
-const port = process.env.SERVER_PORT || 8080;
+const port = process.env.PORT || 8080;
 const router = express.Router();
 
 const mapToFilter = require('./filters')

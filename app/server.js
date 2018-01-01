7305cf8806ade769988c56016761d47dae2a69ab
Valerie Dryden <outragedpinkracoon@gmail.com>
2018-01-01 01:49:37 +0000

    sets up cors

--- app/server.js
+++ app/server.js
@@ -5,6 +5,12 @@ const app = express()
 const port = process.env.PORT || 8080;
 const router = express.Router();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const mapToFilter = require('./filters')
 const {
   orderedBy

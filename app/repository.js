8c3251010eca6545bc8b21f5b457be293c25cecd
Valerie Dryden <outragedpinkracoon@gmail.com>
2018-01-01 16:33:18 +0000

    squishes two functions together

--- app/repository.js
+++ app/repository.js
@@ -17,31 +17,23 @@ const orderedBy = async (filter, params) => {
 
 const buildQuery = (filter, params) => {
   const { selector = `car_attributes -> 'cost' ->> 'price'`, order = 'ASC', castType = 'int' } = filter
-  const { limit = 24, searchType = 'all', make = 'all' } = params
+  const { limit = 24, searchType = null, make = null } = params
 
   return `SELECT car_attributes
           FROM scraped_car
           WHERE (car_attributes ->> 'makeSlug') != 'dacia'
-          ${makeLimit(make)}
-          ${searchTypeLimit(searchType)}
+          ${optionalConstraint('makeSlug', make)}
+          ${optionalConstraint('searchType', searchType)}
           ORDER BY (${selector})::${castType} ${order}
           LIMIT ${limit}`
 }
 
-const searchTypeLimit = (searchType) => {
-  let searchTypeLimit = `AND car_attributes ->> 'searchType' = '${searchType}'`
-  if (searchType == 'all') {
-    searchTypeLimit = ''
+const optionalConstraint = (attribute, value) => {
+  let constraint = ''
+  if (value != null) {
+    constraint = `AND car_attributes ->> '${attribute}' = '${value}'`
   }
-  return searchTypeLimit
-}
-
-const makeLimit = (make) => {
-  let makeLimit = ` AND car_attributes ->> 'makeSlug' = '${make}'`
-  if (make == 'all') {
-    makeLimit = ''
-  }
-  return makeLimit
+  return constraint
 }
 
 module.exports = {
